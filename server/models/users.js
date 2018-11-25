ac7911f604d21174c10479eb0549b9ff9c4a2edc
codycool <cody790410@hotmail.com>
2018-11-25 22:04:56 +0800

    update userSchema middleware

--- server/models/users.js
+++ server/models/users.js
@@ -31,4 +31,40 @@ const UserSchema = new Schema({
     },
 },{timestamps: true})
 
+UserSchema.virtual('password')
+    .set(function(value) {
+        this._password = value
+    })
+    .get(function() {
+        return this._password
+    })
+
+UserSchema.pre('validate',function(next) {
+    if(this.password){
+        if(this.password.length<6){
+            const err = new Boom('Length of password must >= 6',{statusCode: 422,data: 422001})
+            next(err)
+        }
+    }
+    next()
+})
+
+UserSchema.pre('save', async function(next) {
+    if(!this.password) {
+        next()
+    }
+    try{
+        this.hashedPassword = await bcrypt.hash(this.password)
+    }catch(err) {
+        const err = new Boom('Bcrypt error',{statusCode: 500,data: 500001})
+        next(err)
+    }
+})
+
+UserSchema.methods.validatePassword = async (password)=> {
+    return await bcrypt.compare(password,this.hashedPassword)
+}
+
+
+
 export default mongoose.model('User', UserSchema)
